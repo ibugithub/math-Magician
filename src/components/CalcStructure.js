19874f6614928fc314bc75e2fb39b5009cf1b77a
ibugithub <outsideworkibrahim@gmail.com>
2023-06-07 13:49:42 +0600

    Fix some console error

--- src/components/CalcStructure.js
+++ src/components/CalcStructure.js
@@ -4,37 +4,39 @@ const CalcStructure = () => (
       0
     </div>
     <table className="numbers-container">
-      <tr className="numbers">
-        <td>AC</td>
-        <td>+/-</td>
-        <td>%</td>
-        <td className="orangeBg">÷</td>
-      </tr>
-      <tr className="numbers">
-        <td>7</td>
-        <td>8</td>
-        <td>9</td>
-        <td className="orangeBg">&times;</td>
-      </tr>
-      <tr className="numbers">
-        <td>4</td>
-        <td>5</td>
-        <td>6</td>
-        <td className="orangeBg">-</td>
-      </tr>
-      <tr className="numbers">
-        <td>1</td>
-        <td>2</td>
-        <td>3</td>
-        <td className="orangeBg">+</td>
-      </tr>
-      <tr className="numbers">
-        <td colSpan="2">0</td>
-        <td> . </td>
-        <td className="orangeBg">=</td>
-      </tr>
+      <tbody>
+        <tr className="numbers">
+          <td>AC</td>
+          <td>+/-</td>
+          <td>%</td>
+          <td className="orangeBg">÷</td>
+        </tr>
+        <tr className="numbers">
+          <td>7</td>
+          <td>8</td>
+          <td>9</td>
+          <td className="orangeBg">&times;</td>
+        </tr>
+        <tr className="numbers">
+          <td>4</td>
+          <td>5</td>
+          <td>6</td>
+          <td className="orangeBg">-</td>
+        </tr>
+        <tr className="numbers">
+          <td>1</td>
+          <td>2</td>
+          <td>3</td>
+          <td className="orangeBg">+</td>
+        </tr>
+        <tr className="numbers">
+          <td colSpan="2">0</td>
+          <td> . </td>
+          <td className="orangeBg">=</td>
+        </tr>
+      </tbody>
     </table>
-
   </div>
 );
+
 export default CalcStructure;
