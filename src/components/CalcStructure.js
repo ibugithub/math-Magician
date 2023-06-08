cbb5c775102ae20247512be585ea75a9e5c357cb
ibugithub <outsideworkibrahim@gmail.com>
2023-06-08 20:42:39 +0600

    Add the calculator logic to the calculator structure

--- src/components/CalcStructure.js
+++ src/components/CalcStructure.js
@@ -1,42 +1,152 @@
-const CalcStructure = () => (
-  <div>
-    <div className="output-screen">
-      0
+import { useState } from 'react';
+import calculate from '../logic/calculate';
+
+const CalcStructure = () => {
+  const [result, setResult] = useState('0');
+  const [calculatorData, setCalculatorData] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
+
+  const handleButtonClick = (event) => {
+    const buttonValue = event.target.textContent;
+    const updatedCalculatorData = calculate(calculatorData, buttonValue);
+    setCalculatorData(updatedCalculatorData);
+
+    // Update the result display
+    if (updatedCalculatorData.operation) {
+      if (updatedCalculatorData.next) {
+        setResult(`${updatedCalculatorData.total.toString()
+        }  ${updatedCalculatorData.operation.toString()}  ${updatedCalculatorData.next.toString()}`);
+      } else {
+        setResult(`${updatedCalculatorData.total.toString()}  ${updatedCalculatorData.operation}`);
+      }
+    } else if (updatedCalculatorData.next) {
+      setResult(updatedCalculatorData.next.toString());
+    } else if (updatedCalculatorData.total) {
+      setResult(updatedCalculatorData.total.toString());
+    } else {
+      setResult('0');
+    }
+  };
+
+  return (
+    <div>
+      <div className="output-screen">
+        {result}
+      </div>
+      <table className="numbers-container">
+        <tbody>
+          <tr className="numbers">
+            <td>
+              <button type="button" onClick={handleButtonClick}>
+                AC
+              </button>
+            </td>
+            <td>
+              <button type="button" onClick={handleButtonClick}>
+                +/-
+              </button>
+            </td>
+            <td>
+              <button type="button" onClick={handleButtonClick}>
+                %
+              </button>
+            </td>
+            <td className="orangeBg">
+              <button type="button" onClick={handleButtonClick}>
+                ÷
+              </button>
+            </td>
+          </tr>
+          <tr className="numbers">
+            <td>
+              <button type="button" onClick={handleButtonClick}>
+                7
+              </button>
+            </td>
+            <td>
+              <button type="button" onClick={handleButtonClick}>
+                8
+              </button>
+            </td>
+            <td>
+              <button type="button" onClick={handleButtonClick}>
+                9
+              </button>
+            </td>
+            <td className="orangeBg">
+              <button type="button" onClick={handleButtonClick}>
+                x
+              </button>
+            </td>
+          </tr>
+          <tr className="numbers">
+            <td>
+              <button type="button" onClick={handleButtonClick}>
+                4
+              </button>
+            </td>
+            <td>
+              <button type="button" onClick={handleButtonClick}>
+                5
+              </button>
+            </td>
+            <td>
+              <button type="button" onClick={handleButtonClick}>
+                6
+              </button>
+            </td>
+            <td className="orangeBg">
+              <button type="button" onClick={handleButtonClick}>
+                -
+              </button>
+            </td>
+          </tr>
+          <tr className="numbers">
+            <td>
+              <button type="button" onClick={handleButtonClick}>
+                1
+              </button>
+            </td>
+            <td>
+              <button type="button" onClick={handleButtonClick}>
+                2
+              </button>
+            </td>
+            <td>
+              <button type="button" onClick={handleButtonClick}>
+                3
+              </button>
+            </td>
+            <td className="orangeBg">
+              <button type="button" onClick={handleButtonClick}>
+                +
+              </button>
+            </td>
+          </tr>
+          <tr className="numbers">
+            <td colSpan="2">
+              <button type="button" onClick={handleButtonClick}>
+                0
+              </button>
+            </td>
+            <td>
+              <button type="button" onClick={handleButtonClick}>
+                .
+              </button>
+            </td>
+            <td className="orangeBg">
+              <button type="button" onClick={handleButtonClick}>
+                =
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
-    <table className="numbers-container">
-      <tbody>
-        <tr className="numbers">
-          <td>AC</td>
-          <td>+/-</td>
-          <td>%</td>
-          <td className="orangeBg">÷</td>
-        </tr>
-        <tr className="numbers">
-          <td>7</td>
-          <td>8</td>
-          <td>9</td>
-          <td className="orangeBg">&times;</td>
-        </tr>
-        <tr className="numbers">
-          <td>4</td>
-          <td>5</td>
-          <td>6</td>
-          <td className="orangeBg">-</td>
-        </tr>
-        <tr className="numbers">
-          <td>1</td>
-          <td>2</td>
-          <td>3</td>
-          <td className="orangeBg">+</td>
-        </tr>
-        <tr className="numbers">
-          <td colSpan="2">0</td>
-          <td> . </td>
-          <td className="orangeBg">=</td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
-);
+  );
+};
 
 export default CalcStructure;
